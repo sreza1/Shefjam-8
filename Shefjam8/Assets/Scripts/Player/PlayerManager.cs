394882b844dbf145dcfc17427c10c29e44816979
Luke Peacock <lukepeacock13@gmail.com>
2022-02-11 23:45:01 +0000

    Event Manager and Player Manager

---
+++ Shefjam8/Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+
+	[SerializeField] private GameObject playerOne;
+
+	public GameObject GetPlayer() {
+		return playerOne;
+	}
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
