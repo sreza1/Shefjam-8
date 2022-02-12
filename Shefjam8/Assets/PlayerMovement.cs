1b4e4519a4ec987948e8b70a7d15b7a32a11ffa5
Syed Reza <shrezash98@gmail.com>
2022-02-12 01:37:24 +0000

    basic shooting and collision

--- Shefjam8/Assets/PlayerMovement.cs
+++ Shefjam8/Assets/PlayerMovement.cs
@@ -7,15 +7,31 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 99f;
     public Rigidbody2D rb;
     Vector2 movement;
+    Vector2 rotate;
+    Vector2 mousePos;
+
+
+
+
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        //ps5 right analog stick rotation
+        rotate.x = Input.GetAxisRaw("HorizontalTurn");
+        rotate.y = Input.GetAxisRaw("VerticalTurn");
+
+
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement*moveSpeed *Time.fixedDeltaTime);
+
+        float angle = Mathf.Atan2(rotate.y, rotate.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+        
     }
 }
---
+++ Shefjam8/Assets/Shooting.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public float bulletForce = 20f;
+    public float fireDelta = 0.5F;
+
+    private float nextFire = 2f;
+    private float myTime = 0.0F;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        myTime = myTime + Time.deltaTime;
+        if ((Input.GetAxisRaw("HorizontalTurn")!=0 || Input.GetAxisRaw("HorizontalTurn")!=0) && myTime > nextFire)
+        {
+            nextFire = myTime + fireDelta;
+            Shoot();
+
+            nextFire = nextFire - myTime;
+            myTime = 0.0F;
+        }
+        
+    }
+
+    void Shoot(){
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+}
