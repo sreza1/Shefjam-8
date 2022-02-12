69d45015a2eadab60441ea5b3bdf53779b66be28
Luke Peacock <lukepeacock13@gmail.com>
2022-02-12 00:27:58 +0000

    Added PlayerCollided Function to itemBase (set functions to virtual)

--- Shefjam8/Assets/Scripts/Items/ItemBase.cs
+++ Shefjam8/Assets/Scripts/Items/ItemBase.cs
@@ -4,31 +4,32 @@ using UnityEngine;
 
 public class ItemBase : MonoBehaviour
 {
-
 	[SerializeField] private GameObject eventSystem;
 	private PlayerManager playerManager;
 	private GameObject p1 = null;
 
     // Start is called before the first frame update
-    void Start()
+    protected virtual void Start()
     {
         playerManager = eventSystem.GetComponent<PlayerManager>();
         p1 = playerManager.GetPlayer();
     }
 
     // Update is called once per frame
-    void Update()
+    protected virtual void Update()
     {
-        
     }
 
-   	 // Upon collision with another GameObject, check if it is player one, and destroy self (test)
+   	// Upon collision with another GameObject, check if it is player one, and destroy self (test)
     private void OnTriggerEnter2D(Collider2D other)
     {
     	GameObject collidingObject = other.gameObject;
-
     	if (collidingObject == p1) {
-    		Destroy(gameObject);
+    		PlayerCollided(collidingObject);
     	}
+    }
+
+    protected virtual void PlayerCollided(GameObject player) {
+    	Destroy(gameObject);
     }
 }
